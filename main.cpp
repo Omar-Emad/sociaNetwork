b95880007605a609ffa4aca565735c147835fce0
OmarAliAbdelNaby <33624513+OmarAliAbdelNaby@users.noreply.github.com>
2018-04-17 18:05:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "database.h"
-
-
 int main()
 {
-
-
+    file ftest;
+    ftest.setFileName("d:\\data\\users.txt");
+    ftest.writeLine("1,omar,m,25");
+	ftest.writeLine("2,ahmed,p,29");
+    cout << ftest.readLine(0, "1");
     return 0;
 }
