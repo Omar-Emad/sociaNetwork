ec210e510c37a2ec7bbd81b06c35323feb8218d7
Kareem991 <33132588+Kareem991@users.noreply.github.com>
2018-04-17 21:41:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <database.h>
 using namespace std;
 void New_account(){
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 SetConsoleTextAttribute(hConsole, 5);
-cout<<"please enter your username: "<<endl;
-cout<<"please enter your password: "<<endl;
-cout<<"please enter your gender: "<<endl;
-cout<<"please enter your age: "<<endl;
+cout<<endl;
+cout<<"please enter your username: "<<endl<<endl;
+cout<<"please enter your password: "<<endl<<endl;
+cout<<"please enter your gender: "<<endl<<endl;
+cout<<"please enter your age: "<<endl<<endl;
 char dummy;
 dummy=getch();
 system("CLS");
@@ -19,13 +21,15 @@ system("CLS");
 void Welcome(){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, 2);
-cout<<"Welcome, (username)!"<<endl;
-cout<<"What do you want to do today (please type a number corresponding to one of the following options:)?"<<endl;
+cout<<"Welcome, (username)!"<<endl<<endl;
+cout<<"What do you want to do today? (please type a number corresponding to one of the following options:)"<<endl;
+cout<<endl;
 SetConsoleTextAttribute(hConsole, 5);
-cout <<"1) Add a new friend." << endl;
-cout <<"2) Write a new post." << endl;
-cout <<"3) View your friends." << endl;
-cout <<"4) View someone's posts." << endl;
+cout<<endl<<endl;
+cout <<"1) Add a new friend." << endl << endl;
+cout <<"2) Write a new post." << endl << endl;
+cout <<"3) View your friends." << endl << endl;
+cout <<"4) View someone's posts." << endl << endl;
 char dummy;
 dummy=getch();
 system("CLS");
@@ -37,8 +41,9 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 SetConsoleTextAttribute(hConsole, 2);
 cout << "Hello! (please type a number corresponding to one of the following options:)" << endl;
 SetConsoleTextAttribute(hConsole, 5);
-    cout <<"1) Create a new account." << endl;
-     cout <<"2) Login to an existing account." << endl;
+cout<<endl<<endl;
+    cout <<"1) Create a new account." << endl<<endl ;
+     cout <<"2) Login to an existing account." << endl << endl;
      char choice;
      choice=getch();
      system("CLS");
