fd2b6e49f9c8af04b8aabd5a117ca791c432965c
OmarAliAbdelNaby <33624513+OmarAliAbdelNaby@users.noreply.github.com>
2018-04-30 17:07:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 
 int main()
 {
-    file ftest;
-    ftest.setFileName("d:\\data\\users.txt");
-    ftest.writeLine("1,omar,m,25");
-	ftest.writeLine("2,ahmed,p,29");
-    cout << ftest.readLine(0, "1");
+    generateXML();
     return 0;
 }
